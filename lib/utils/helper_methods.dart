180ee2101fccf31a90e05e8d05bdcdab9c3d3fe9
uemirhanselim <uemirhanselim@gmail.com>
2024-02-18 23:44:41 +0300

    helpers

--- lib/utils/helper_methods.dart
+++ lib/utils/helper_methods.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 double getDynamicHeight(BuildContext context, double value) =>
     MediaQuery.of(context).size.height * value;
@@ -7,5 +8,16 @@ double getDynamicWidth(BuildContext context, double value) =>
 
 SizedBox emptySpaceHeight(BuildContext context, double value) => SizedBox(
     height: MediaQuery.of(context).size.height * value, child: const Text(''));
-SizedBox emptySpaceWidth(BuildContext context, double value) =>
-    SizedBox(width: MediaQuery.of(context).size.width * value, child: const Text(''));
+SizedBox emptySpaceWidth(BuildContext context, double value) => SizedBox(
+    width: MediaQuery.of(context).size.width * value, child: const Text(''));
+
+// Date Formatter
+
+String dateFormatterMonthDay(DateTime? date) {
+  final formatter = DateFormat("MMM d");
+  return formatter.format(date!);
+}
+
+String dateFormatterYearMonthDay(DateTime date) {
+  return DateFormat('yyyy-MM-dd').format(date);
+}
