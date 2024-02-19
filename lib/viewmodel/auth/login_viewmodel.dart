aeaabbb9e88a27925dda2c3ecf34f0e15c491cb3
uemirhanselim <uemirhanselim@gmail.com>
2024-02-19 20:11:11 +0300

    viewmodel update

--- lib/viewmodel/auth/login_viewmodel.dart
+++ lib/viewmodel/auth/login_viewmodel.dart
@@ -1,16 +1,21 @@
 import 'dart:convert';
 
 import 'package:flutter/material.dart';
+import 'package:service_tak_mobile/locator.dart';
 import 'package:service_tak_mobile/model/auth/login_model.dart';
 import 'package:service_tak_mobile/model/helper/helper_model.dart';
 import 'package:service_tak_mobile/service/auth/login_service.dart';
+import 'package:service_tak_mobile/service/local/local_storage_service.dart';
+import 'package:service_tak_mobile/utils/local_storage_keys.dart';
 import 'package:service_tak_mobile/utils/navigation_helper.dart';
-import 'package:service_tak_mobile/view/security/security_product_detail_screen.dart';
-import 'package:service_tak_mobile/view/spa/spa_product_detail_screen.dart';
+import 'package:service_tak_mobile/view/reception/reception_open_screen.dart';
+import 'package:service_tak_mobile/view/worker/scan_barcode_screen.dart';
 
 class LoginViewModel extends ChangeNotifier {
+  final LoginService _loginService = locator.get<LoginService>();
+
   LoginViewModel() {
-    print("Login VM Called");
+    debugPrint("Login VM Called");
   }
 
   //Setters
@@ -59,23 +64,35 @@ class LoginViewModel extends ChangeNotifier {
   }
 
   Future<void> userLogin(BuildContext context) async {
-    var response = await LoginService()
-        .userLogin(_emailController.text, passwordController.text);
+    var response = await _loginService.userLogin(
+        _emailController.text, passwordController.text);
     setIsLoading = false;
 
     if (response.statusCode == 200) {
+      if (_isChecked) {
+        await LocalStorageService.instance
+            .setBool(LocalStorageKeys.isUserLoggedIn, true);
+      }
+
       LoginResponseModel responseData =
           LoginResponseModel.fromJson(json.decode(response.body));
+      await LocalStorageService.instance
+          .setString(LocalStorageKeys.userType, responseData.type!);
+      await LocalStorageService.instance
+          .setString(LocalStorageKeys.userAuthToken, responseData.token!);
 
       if (responseData.type == "reception") {
         debugPrint("---- Reception LOGIN -----");
-        // navigatorPush(context, otelscreen);
+        if (!context.mounted) return;
+        await navigatorPush(context, const ReceptionOpenScreen());
       } else if (responseData.type == "spa") {
         debugPrint("---- Spa LOGIN  -----");
-        navigatorPush(context, SpaProductDetailScreen());
+        if (!context.mounted) return;
+        await navigatorPush(context, const ScanBarcodeScreen());
       } else if (responseData.type == "security") {
         debugPrint("---- Security LOGIN -----");
-        navigatorPush(context, SecurityProductDetailScreen());
+        if (!context.mounted) return;
+        await navigatorPush(context, const ScanBarcodeScreen());
       } else {
         setHasError = true;
         setErrorMessage = "Problem with login! Please try again.";
