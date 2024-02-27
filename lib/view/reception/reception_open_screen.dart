ac42303e0b26268d63dc5902e3dd2730c2416236
uemirhanselim <uemirhanselim@gmail.com>
2024-02-27 14:51:14 +0300

    reception open screen boxfit changed to contain

--- lib/view/reception/reception_open_screen.dart
+++ lib/view/reception/reception_open_screen.dart
@@ -265,7 +265,7 @@ class ReceptionOpenScreen extends StatelessWidget {
             borderRadius: BorderRadius.circular(8),
             child: CachedNetworkImage(
               imageUrl: viewModel.hotel?.logo ?? "",
-              fit: BoxFit.cover,
+              fit: BoxFit.contain,
               placeholder: (context, _) {
                 return const Center(
                   child: CircularProgressIndicator(
